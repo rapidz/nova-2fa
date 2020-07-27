f49ce3af95c3f23329c10030a1ba0751cb2e45a9
Gareth Somers <garethsomers3@gmail.com>
2020-07-27 14:10:30 +0100

    Use screen2fa over google2fa

--- src/Google2fa.php
+++ src/Google2fa.php
@@ -31,7 +31,7 @@ class Google2fa extends Tool
 
         $google2fa = new G2fa();
 
-        return $google2fa->verifyKey(auth()->user()->user2fa->google2fa_secret, $secret, config('google2fa.window'));
+        return $google2fa->verifyKey(auth()->user()->user2fa->google2fa_secret, $secret, config('screen2fa.window'));
     }
 
     /**
